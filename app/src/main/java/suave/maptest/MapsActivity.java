4fd7df64dafb0e7f3469acf102cc4d790b1f2780
mattcbb <mciar2@unh.newhaven.edu>
2014-10-15 01:58:11 -0400

    Image overlay
    Overlay images onto Google Maps

--- app/src/main/java/suave/maptest/MapsActivity.java
+++ app/src/main/java/suave/maptest/MapsActivity.java
@@ -8,10 +8,23 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
+import java.net.URL;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import android.os.AsyncTask;
+
 
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    LatLng HOME = new LatLng(41.291402, -72.960382);// Original (UNH): (41.291402, -72.961182)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +32,15 @@ public class MapsActivity extends FragmentActivity {
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
         goToUNH();
+
+        try {
+            URL bkmn = new URL("http://img1.wikia.nocookie.net/__cb20120128201030/muppet/images/9/92/Bigbirdnewversion.png");  // Image location of Buckman floor 2
+            new DownloadBMP(mMap).execute(bkmn);
+        }
+        catch (IOException e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -69,7 +91,39 @@ public class MapsActivity extends FragmentActivity {
     // My functions start here
 
     private void goToUNH() {
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(41.291402, -72.961182), 14.0f));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(HOME, 18.0f));
     }
 
+}
+
+
+class DownloadBMP extends AsyncTask<URL, Void, Bitmap> {
+    Bitmap map;
+    GoogleMap googleM;
+    LatLng HOME = new LatLng(41.291402, -72.960382);
+
+    public DownloadBMP(GoogleMap g) {
+        this.googleM = g;
+    }
+
+    protected Bitmap doInBackground(URL... u) {
+        Bitmap bit = null;
+        try {
+            bit = BitmapFactory.decodeStream(u[0].openConnection().getInputStream());
+        }
+        catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+        return bit;
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap result) {
+        super.onPostExecute(result);
+        GroundOverlayOptions BuckmanMap = new GroundOverlayOptions()
+                .image(BitmapDescriptorFactory.fromBitmap(result))
+                .position(HOME, 100f);
+        googleM.addGroundOverlay(BuckmanMap);
+    }
 }
